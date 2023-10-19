f9209e1610e2680eb856478b95ae17c337f8031a
Gabriel Massensini <137442121+GabrielMassensini@users.noreply.github.com>
2023-10-19 01:44:09 -0300

    Basic results already appear on the screen

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 const CURRENT_DATE = new Date();
 const CURRENT_YEAR = CURRENT_DATE.getFullYear();
-const CURRENT_MOUNTH = CURRENT_DATE.getMonth() + 1;
+const CURRENT_MONTH = CURRENT_DATE.getMonth() + 1;
 const CURRENT_DAY = CURRENT_DATE.getDate();
 
 const YEAR_OF_BIRTH = document.getElementById("year").value;
-const MOUNTH_OF_BIRTH = document.getElementById("month").value;
+const MONTH_OF_BIRTH = document.getElementById("month").value;
 const DAY_OF_BIRTH = document.getElementById("day").value;
 
 const YEARS_OF_LIVE = CURRENT_YEAR - YEAR_OF_BIRTH;
-const MOUNTHS_OF_LIVE = CURRENT_MOUNTH - MOUNTH_OF_BIRTH;
+const MONTHS_OF_LIVE = CURRENT_MONTH - MONTH_OF_BIRTH;
 const DAYS_OF_LIVE = CURRENT_DAY - DAY_OF_BIRTH - 1;
 
+const YEARS_RESULT = document.getElementById("year-result");
+const MONTH_RESULT = document.getElementById("month-result");
+const DAY_RESULT = document.getElementById("day-result");
 
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  
+  YEARS_RESULT.innerHTML = YEARS_OF_LIVE;
+  MONTH_RESULT.innerHTML = MONTHS_OF_LIVE;
+  DAY_RESULT.innerHTML = DAYS_OF_LIVE;
+});
