7c1652af89a27a6110c90ab03dd803cea9a03bc8
Gabriel Massensini <137442121+GabrielMassensini@users.noreply.github.com>
2023-10-23 03:13:24 -0300

    funcionalidades implementadas

--- main.js
+++ main.js
@@ -7,10 +7,22 @@ const YEAR_OF_BIRTH = document.getElementById("year");
 const MONTH_OF_BIRTH = document.getElementById("months");
 const DAYS_OF_BIRTH = document.getElementById("days");
 
+const YOUR_BIRTH = document.getElementById("your-birth");
+const YOUR_AGE = document.getElementById("your-age");
+const DAYS_LIVED = document.getElementById("days-lived");
+const HOURS_LIVED = document.getElementById("hours-lived");
+const MINUTES_LIVED = document.getElementById("minutes-lived");
+const MILLISECONDS_LIVED = document.getElementById("milliseconds-lived");
+
+const BTN = document.getElementById("btn");
+
 function inputYearCheck() {
-  if (MONTH_OF_BIRTH.value !== "") {
+  if (YEAR_OF_BIRTH.value !== "") {
     MONTH_OF_BIRTH.removeAttribute("disabled");
     DAYS_OF_BIRTH.removeAttribute("disabled");
+  } else {
+    MONTH_OF_BIRTH.setAttribute("disabled", "disabled");
+    DAYS_OF_BIRTH.setAttribute("disabled", "disabled");
   }
 }
 
@@ -18,6 +30,8 @@ function howManyDays() {
   const SELECTED_MONTH = MONTH_OF_BIRTH.value;
 
   let maxDays = 31;
+
+  DAYS_OF_BIRTH.innerHTML = "";
 
   switch (SELECTED_MONTH) {
     case "April":
@@ -43,3 +57,35 @@ function howManyDays() {
     DAYS_OF_BIRTH.appendChild(OPTION);
   }
 }
+
+function calculated() {
+  const DATE = new Date(
+    YEAR_OF_BIRTH.value,
+    MONTH_OF_BIRTH.value - 1,
+    DAYS_OF_BIRTH.value
+  );
+
+  const TOTAL_YEAR = CURRENT_YEAR - YEAR_OF_BIRTH.value;
+  const TOTAL_MONTH = CURRENT_MONTH - MONTH_OF_BIRTH.value;
+  const TOTAL_DAYS = CURRENT_DAY - DAYS_OF_BIRTH.value;
+  const TOTAL_DAYS_OF_LIFE = Math.floor(
+    (CURRENT_DATE - DATE) / (1000 * 60 * 60 * 24)
+  );
+  const TOTAL_HOURS_OF_LIFE = Math.floor(
+    (CURRENT_DATE - DATE) / (1000 * 60 * 60)
+  );
+  const TOTAL_MINUTES_OF_LIFE = Math.floor((CURRENT_DATE - DATE) / (1000 * 60));
+  const TOTAL_MILLISECONDS_OF_LIFE = Math.floor(CURRENT_DATE - DATE);
+
+  YOUR_BIRTH.innerHTML = `${TOTAL_MONTH}/${TOTAL_DAYS}/${TOTAL_YEAR}`;
+  YOUR_AGE.innerHTML = `${TOTAL_YEAR} years, ${TOTAL_MONTH} months and ${TOTAL_DAYS} days of life`;
+  DAYS_LIVED.innerHTML = `${TOTAL_DAYS_OF_LIFE} days`;
+  HOURS_LIVED.innerHTML = `${TOTAL_HOURS_OF_LIFE} hours`;
+  MINUTES_LIVED.innerHTML = `${TOTAL_MINUTES_OF_LIFE} minutes`;
+  MILLISECONDS_LIVED.innerHTML = `${TOTAL_MILLISECONDS_OF_LIFE} milliseconds`;
+}
+
+BTN.addEventListener("click", (e) => {
+  e.preventDefault();
+  calculated();
+});
